6fc206c0ad3813c4a6a911a56bd27806dab07279
ccall <c.calli@diten.tech>
2023-01-19 11:05:45 +0300

    contact api

---
+++ app/Http/Controllers/Api/ContactController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contacts = Contact::all();
+        return $contacts;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $contact = new Contact();
+        $contact->name = $request->name;
+        $contact->email = $request->email;
+        $contact->phone = $request->phone;
+        $contact->message = $request->message;
+
+        $contact->save();
+        return response()->json(['message'=>'mesaj kayıt işlemi başarılı...']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $contact = Contact::find($id);
+        if ($contact == null){
+            return response()->json(['message'=>'Aradığınız mesaj bulunamadı...'],404);
+        }
+        return $contact;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $contact = Contact::findOrFail($id);
+        $contact->name = $request->name;
+        $contact->email = $request->email;
+        $contact->phone = $request->phone;
+        $contact->message = $request->message;
+        $contact->save();
+        return $contact;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $contact = Contact::destroy($id);
+        return response()->json(['message'=>'Mesaj silme işlemi başarılı...']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -21,41 +21,49 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::controller(\App\Http\Controllers\Api\ItemController::class)->group(function () {
-    Route::get('/items','index');
-    Route::get('/item/{id}','show');
-    Route::post('/item','store');
+    Route::get('/items','index')->middleware(['jwt']);
+    Route::get('/item/{id}','show')->middleware(['jwt']);
+    Route::post('/item','store')->middleware(['jwt']);
     Route::put('/item/{id}','update')->middleware(['jwt']);
     Route::delete('/item/{id}','destroy')->middleware(['jwt']);
 });
 
+Route::controller(\App\Http\Controllers\Api\ContactController::class)->group(function () {
+    Route::get('/contacts','index')->middleware(['jwt']);
+    Route::get('/contact/{id}','show')->middleware(['jwt']);
+    Route::post('/contact','store')->middleware(['jwt']);
+    Route::put('/contact/{id}','update')->middleware(['jwt']);
+    Route::delete('/contact/{id}','destroy')->middleware(['jwt']);
+});
+
 Route::controller(\App\Http\Controllers\Api\PagesController::class)->group(function () {
-    Route::get('/pages','index');
-    Route::get('/page/{id}','show');
-    Route::post('/page','store');
+    Route::get('/pages','index')->middleware(['jwt']);
+    Route::get('/page/{id}','show')->middleware(['jwt']);
+    Route::post('/page','store')->middleware(['jwt']);
     Route::put('/page/{id}','update')->middleware(['jwt']);
     Route::delete('/page/{id}','destroy')->middleware(['jwt']);
 });
 
 Route::controller(\App\Http\Controllers\Api\ArticleController::class)->group(function () {
-    Route::get('/articles','index');
-    Route::get('/article/{id}','show');
-    Route::post('/article','store');
+    Route::get('/articles','index')->middleware(['jwt']);
+    Route::get('/article/{id}','show')->middleware(['jwt']);
+    Route::post('/article','store')->middleware(['jwt']);
     Route::put('/article/{id}','update')->middleware(['jwt']);
     Route::delete('/article/{id}','destroy')->middleware(['jwt']);
 });
 
 Route::controller(\App\Http\Controllers\Api\CategoryController::class)->group(function () {
-    Route::get('/categories','index');
-    Route::get('/category/{id}','show');
-    Route::post('/category','store');
+    Route::get('/categories','index')->middleware(['jwt']);
+    Route::get('/category/{id}','show')->middleware(['jwt']);
+    Route::post('/category','store')->middleware(['jwt']);
     Route::put('/category/{id}','update')->middleware(['jwt']);
     Route::delete('/category/{id}','destroy')->middleware(['jwt']);
 });
 
 Route::controller(\App\Http\Controllers\Api\UserController::class)->group(function () {
-    Route::get('/users','index');
-    Route::get('/user/{id}','show');
-    Route::post('/user','store');
+    Route::get('/users','index')->middleware(['jwt']);
+    Route::get('/user/{id}','show')->middleware(['jwt']);
+    Route::post('/user','store')->middleware(['jwt']);
     Route::put('/user/{id}','update')->middleware(['jwt']);
     Route::delete('/user/{id}','destroy')->middleware(['jwt']);
 });
